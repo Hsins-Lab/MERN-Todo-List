24434b1e546440a8929df251d0c11b6d149fc927
Hsins <hsinspeng@gmail.com>
2019-07-04 22:01:48 +0800

    Replace the tabs with spaces.

--- routes/todos.js
+++ routes/todos.js
@@ -5,7 +5,7 @@
 |
 | [Method]  [Route]
 | GET	      /api/todos	            取得所有待辦
-| POST	    /api/todos	            新增一件待辦
+| POST      /api/todos	            新增一件待辦
 | GET	      /api/todos/:id	        顯示待辦事項
 | POST	    /api/todos/:id	        完成待辦事項／取消待辦事項
 | PATCH     /api/todos/:id  	      更新待辦事項
